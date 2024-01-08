1f59d7a6d40bb8b56e7a8e801de12fe66f623ef0
quiveringbacon <128983862+quiveringbacon@users.noreply.github.com>
2024-01-08 09:55:17 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -632,7 +632,7 @@ resource "azurerm_subnet_route_table_association" "onspoke1defaultsubnet" {
   timeouts {
     create = "2h"
     read = "2h"
-    update = "2h"
+    #update = "2h"
     delete = "2h"
   }
 }
@@ -642,7 +642,7 @@ resource "azurerm_subnet_route_table_association" "onspoke2defaultsubnet" {
   timeouts {
     create = "2h"
     read = "2h"
-    update = "2h"
+    #update = "2h"
     delete = "2h"
   }
 }
@@ -652,7 +652,7 @@ resource "azurerm_subnet_route_table_association" "ononpremdefaultsubnet" {
   timeouts {
     create = "2h"
     read = "2h"
-    update = "2h"
+    #update = "2h"
     delete = "2h"
   }
 }
